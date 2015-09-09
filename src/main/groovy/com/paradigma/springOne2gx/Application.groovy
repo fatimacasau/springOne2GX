60cc08ec7b26af869c472b183817a4bdc5583819
Fatima Casau <fcasau@paradigmatecnologico.com>
2015-09-10 00:58:46 +0200

    first commit to step Anywhere

--- src/main/groovy/com/paradigma/springOne2gx/Application.groovy
+++ src/main/groovy/com/paradigma/springOne2gx/Application.groovy
@@ -2,12 +2,39 @@ package com.paradigma.springOne2gx
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ConfigurableApplicationContext
 
 @SpringBootApplication
 class Application {
 
 	static void main(String[] args) {
 
-        	SpringApplication.run Application, args
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class)
+
+		CustomerRepository repository = context.getBean(CustomerRepository.class)
+
+		// save a couple of customers
+		[[firstName:"Jack", lastName:"Bauer"],
+		 [firstName:"Chloe", lastName:"O'Brian"],
+		 [firstName:"Kim", lastName:"Bauer"],
+		 [firstName:"David", lastName:"Palmer"],
+		 [firstName:"Michelle", lastName:"Dessler"]].each {
+			repository.save(new Customer(it))
+		}
+
+		// fetch all customers
+		Iterable<Customer> customers = repository.findAll()
+		println "Customers found with findAll(): \n-------------------------------"
+		customers.each {
+			println it
+		}
+		// fetch an individual customer by ID
+		Customer customer = repository.findOne(1L);
+		println "Customer found with findOne(1L): $customer \n"
+
+		// fetch customers by last name
+		List<Customer> bauers = repository.findByLastName("Bauer");
+		println "Customer found with findByLastName('Bauer'): \n--------------------------------------------"
+		bauers.each {println it}
 	}
 }
---
+++ src/main/groovy/com/paradigma/springOne2gx/CustomerController.groovy
@@ -0,0 +1,25 @@
+package com.paradigma.springOne2gx
+
+import com.paradigma.springOne2gx.Customer
+import com.paradigma.springOne2gx.CustomerRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+/**
+ * Created by fcasau on 1/28/15.
+ */
+@Controller
+public class CustomerController {
+
+    @Autowired
+    CustomerRepository customerRepository
+
+    @RequestMapping("/customer/{id}")
+    @ResponseBody
+    public Customer findById(@PathVariable Long id) {
+        return customerRepository.findOne(id)
+    }
+}

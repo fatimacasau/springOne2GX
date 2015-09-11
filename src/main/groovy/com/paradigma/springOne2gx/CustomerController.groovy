5d342e00a26b1cd5eca407aa0f9ec314d7d8b054
Fatima Casau <fcasau@paradigmatecnologico.com>
2015-09-11 17:22:29 +0200

    remove unneeded imports

--- src/main/groovy/com/paradigma/springOne2gx/CustomerController.groovy
+++ src/main/groovy/com/paradigma/springOne2gx/CustomerController.groovy
@@ -1,7 +1,5 @@
 package com.paradigma.springOne2gx
 
-import com.paradigma.springOne2gx.Customer
-import com.paradigma.springOne2gx.CustomerRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable

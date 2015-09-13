cd712c3c895265c6184c342b29a62bdd05306b04
Fatima Casau <fatimacasau@gmail.com>
2015-09-13 18:09:53 +0200

    removing ;

--- src/main/groovy/com/paradigma/springOne2gx/CustomerRepository.groovy
+++ src/main/groovy/com/paradigma/springOne2gx/CustomerRepository.groovy
@@ -1,15 +1,16 @@
-package com.paradigma.springOne2gx;
+package com.paradigma.springOne2gx
 
-import com.paradigma.springOne2gx.Customer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import com.paradigma.springOne2gx.Customer
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-import java.util.List;
+import java.util.List
 
 /**
  * Created by fcasau on 1/28/15.
  */
 @RepositoryRestResource(collectionResourceRel = "customers", path = "customerRepository")
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
-    List<Customer> findByLastName(String lastName);
+    List<Customer> findByLastName(@Param(value = "lastName") String lastName)
 }
